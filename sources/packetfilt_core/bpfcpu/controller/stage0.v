81f2cce0e24d92ea240a8b97c721a2aa5e5672be
Marco Merlini <mahkoe@gmail.com>
2019-11-17 13:33:26 -0500

    Still editing...

--- sources/packetfilt_core/bpfcpu/controller/stage0.v
+++ sources/packetfilt_core/bpfcpu/controller/stage0.v
@@ -11,7 +11,7 @@ module stage0 (
     input wire rst,
 
     input wire branch_mispredict,
-    output wire rd_en,
+    output wire inst_rd_en,
     output wire PC_en,
     
     input wire next_rdy,
@@ -22,13 +22,13 @@ module stage0 (
     //However, if the branch_mispredict signal is asserted, it means PC is being
     //changed on this cycle, and we should wait
     
-    assign rd_en = next_rdy && !branch_mispredict;
-    assign PC_en = rd_en;
+    assign inst_rd_en = next_rdy && !branch_mispredict;
+    assign PC_en = inst_rd_en;
     
     reg vld_r = 0;
     always @(posedge clk) begin
         if (rst || branch_mispredict) vld_r <= 0;
-        else vld_r <= rd_en;
+        else vld_r <= inst_rd_en;
     end
     
     assign vld = vld_r;
