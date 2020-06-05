ec6a855fc76029bc1469d8ae299a4b623df9f472
Marco Merlini <mahkoe@gmail.com>
2020-06-05 10:24:27 -0400

    Added forwarder width converter. Now, to hook it all up...

---
+++ sources/forwarders/axistream_forwarder/fwd_width_adapter/fwd_width_adapter.v
@@ -0,0 +1,80 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+/*
+
+fwd_width_adapter.v
+
+The interal packet memory must have a minimum port width of 32, which means
+that both ports together make 64 bits. However, it should be possible for a
+narrower snooper/forwarder to use the packet memory. These width converters
+take care of that
+
+This module works in a similar way to the cpu_adapter in the P3 system, but
+is quite a bit easier because we never have to straddle two words.
+
+*/
+
+`ifdef ICARUS_VERILOG
+`define localparam parameter
+`else /* For Vivado */
+`define localparam localparam
+`endif
+
+module fwd_width_adapter # (
+    parameter MEM_WIDTH = 64,
+    parameter FWD_WIDTH = 32,
+    parameter MEM_ADDR_WIDTH = 9,
+    parameter FWD_ADDR_WIDTH = 10,
+    parameter MEM_LAT = 1
+) (
+    input wire clk,
+    
+    //Interface to forwarder
+    input wire [FWD_ADDR_WIDTH-1:0] fwd_addr,
+    output wire [FWD_WIDTH-1:0] fwd_rd_data,
+    output wire fwd_rd_data_vld,
+    
+    //Interface to packet mem
+    output wire [MEM_ADDR_WIDTH-1:0] mem_addr,
+    input wire [MEM_WIDTH-1:0] mem_rd_data,
+    input wire mem_rd_data_vld
+);
+    
+    `localparam N = FWD_ADDR_WIDTH - MEM_ADDR_WIDTH;
+    `localparam RATIO = MEM_WIDTH/FWD_WIDTH;
+    genvar i;
+    
+//Double-check that MEM_WIDTH is a power-of-2 multiple of FWD_WIDTH
+generate if ((MEM_WIDTH/FWD_WIDTH) != 2**N || (MEM_WIDTH % FWD_WIDTH) != 0) begin
+    assign mem_width_must_be_power_of_two_multiple_of_fwd_width = 0;
+end endgenerate
+    
+    //Offset into bigword where next value is read from
+    //We need to hold onto each offset for MEM_LAT cycles
+    wire [N -1:0] offset = fwd_addr[N -1:0];
+    reg [N -1:0] offset_r[0: MEM_LAT - 1];
+generate for (i = 0; i < MEM_LAT; i = i + 1) begin
+    initial offset_r[i] = 0;
+    if (i == 0) begin
+        always @(posedge clk) offset_r[i] = offset;
+    end else begin
+        always @(posedge clk) offset_r[i] = offset_r[i - 1];
+    end
+end endgenerate
+    
+    //Assign outputs
+    reg [FWD_WIDTH -1 :0] segments[0: RATIO-1];
+    generate for (i = 0; i < RATIO; i = i+1) begin
+        assign segments[i] = mem_rd_data[(i+1)*FWD_WIDTH-1 -: FWD_WIDTH];
+    end endgenerate
+    
+    assign mem_addr = fwd_addr[FWD_ADDR_WIDTH -1: N];
+    
+    //Endianness... UGH!
+    assign fwd_rd_data = segments[(RATIO-1) - offset_r[MEM_LAT-1]];
+    assign fwd_rd_data_vld = mem_rd_data_vld;
+    
+endmodule
+
+`undef localparam
---
+++ sources/forwarders/axistream_forwarder/fwd_width_adapter/fwd_width_adapter_tb.v
@@ -0,0 +1,140 @@
+`timescale 1ns / 1ps
+
+`include "fwd_width_adapter.v"
+
+
+`ifdef ICARUS_VERILOG
+`define localparam parameter
+`else /* For Vivado */
+`define localparam localparam
+`endif
+
+module fwd_width_adapter_tb # (
+    parameter MEM_WIDTH = 64,
+    parameter FWD_WIDTH = 32,
+    parameter MEM_ADDR_WIDTH = 9,
+    parameter FWD_ADDR_WIDTH = 10,
+    parameter MEM_LAT = 1
+) ();
+
+    reg clk = 0;
+    
+    //Interface to forwarder
+    reg [FWD_ADDR_WIDTH-1:0] fwd_addr = 0;
+    wire [FWD_WIDTH-1:0] fwd_rd_data;
+    wire fwd_rd_data_vld;
+    
+    //Interface to packet mem
+    wire [MEM_ADDR_WIDTH-1:0] mem_addr;
+    wire [MEM_WIDTH-1:0] mem_rd_data;
+    wire mem_rd_data_vld;
+    
+    reg rd_en = 0;
+    always @(posedge clk) rd_en <= $random;
+    
+    integer fd, dummy, i;
+    
+    initial begin
+        $dumpfile("fwd_width_adapter.vcd");
+        $dumpvars;
+        $dumplimit(512000);
+        
+        //Controlled test
+        for (i = 0; i < 16; i = i + 1) begin
+            @(posedge clk);
+            if(rd_en) begin
+                fwd_addr <= i;
+            end else begin
+                i = i - 1;
+            end
+        end
+        
+        //Fuzz test
+        repeat(40) begin
+            @(posedge clk);
+            fwd_addr <= $random;
+        end
+        
+        #1000 $finish;
+    end
+    
+    always #5 clk <= ~clk;
+
+    fwd_width_adapter # (
+		.MEM_WIDTH(MEM_WIDTH),
+		.FWD_WIDTH(FWD_WIDTH),
+		.MEM_ADDR_WIDTH(MEM_ADDR_WIDTH),
+		.FWD_ADDR_WIDTH(FWD_ADDR_WIDTH),
+		.MEM_LAT(MEM_LAT)
+    ) DUT (
+		.clk(clk),
+        
+        //Interface to forwarder
+		.fwd_addr(fwd_addr),
+		.fwd_rd_data(fwd_rd_data),
+		.fwd_rd_data_vld(fwd_rd_data_vld),
+        
+        //Interface to packet mem
+		.mem_addr(mem_addr),
+		.mem_rd_data(mem_rd_data),
+		.mem_rd_data_vld(mem_rd_data_vld)
+    );
+    
+    fakemem # (
+        .ADDR_WIDTH(MEM_ADDR_WIDTH),
+        .DATA_WIDTH(MEM_WIDTH),
+        .MEM_LAT(MEM_LAT)
+    ) phony (
+        .clk(clk),
+        
+        .addr(mem_addr),
+        .rd_en(rd_en),
+        
+        .data(mem_rd_data),
+        .data_vld(mem_rd_data_vld)
+    );
+
+endmodule
+
+module fakemem # (
+    parameter ADDR_WIDTH = 9,
+    parameter DATA_WIDTH = 64,
+    parameter MEM_LAT = 1
+) (
+    input wire clk,
+    
+    input wire [ADDR_WIDTH - 1:0] addr,
+    input wire rd_en,
+    
+    output wire [DATA_WIDTH -1:0] data,
+    output wire data_vld
+);
+    
+    `localparam MEM_DEPTH = 2**ADDR_WIDTH * DATA_WIDTH;
+    reg [0: MEM_DEPTH -1] mem;
+    genvar i;
+generate for (i = 0; i < MEM_DEPTH/8; i = i + 1) begin
+    initial mem[8*(i+1) -1 -: 8] = i & 8'hFF;
+end endgenerate
+    
+    //Delay rd_en to produce data_vld signal
+    reg rd_en_r[0: MEM_LAT -1];
+    always @(posedge clk) rd_en_r[0] <= rd_en;
+generate for (i = 1; i < MEM_LAT; i = i + 1) begin
+    always @(posedge clk) rd_en_r[i] <= rd_en_r[i - 1];
+end endgenerate
+    
+    //Pretend to be memory with the proper latency
+    reg [DATA_WIDTH -1:0] data_r[0: MEM_LAT - 1];
+    always @(posedge clk) data_r[0] <= mem[DATA_WIDTH*(addr+1)-1 -: DATA_WIDTH];
+generate for (i = 1; i < MEM_LAT; i = i + 1) begin
+    always @(posedge clk) data_r[i] <= data_r[i - 1];
+end endgenerate
+    
+    //Assign outputs
+    assign data = data_r[MEM_LAT - 1];
+    assign data_vld = rd_en_r[MEM_LAT - 1];
+    
+endmodule
+
+`undef localparam
